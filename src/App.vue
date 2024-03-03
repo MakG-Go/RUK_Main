d3e440cc4aba42f651a23cbfa3c168b14690de31
MakG-Go <92160024+MakG-Go@users.noreply.github.com>
2024-03-03 22:49:22 +0300

    Correct APP

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,6 @@
 <template>
     <div>
-        <!-- Сплеш -->
-        <!-- 
-        <transition name="splash">
-            <Splash v-if="!start"></Splash>
-        </transition> -->
-
-        <!-- <Header> </Header> -->
-
         <router-view></router-view>
-
-        <!-- <router-view v-slot="{ Component }">
-            <transition
-                mode="out-in"
-                :name="$route.meta.transition || 'navigation'"
-            >
-                <component :is="Component" :key="$route.path"></component>
-            </transition>
-        </router-view> -->
-
-        <!-- <div class="wrapper-course" :class="getMainWrapper">
-            <Header> </Header>
-
-            <perfect-scrollbar
-                v-if="start"
-                ref="globalScroll"
-                @ps-scroll-y="onGlobalScroll"
-                class="js-global-scroll"
-            >
-                <div class="container" :class="getMainContainer">
-                    <router-view v-slot="{ Component }">
-                        <transition
-                            appear
-                            mode="out-in"
-                            appear-active-class="animate__animated animate__fadeIn"
-                            name="navigation"
-                        >
-                            <component
-                                :is="Component"
-                                :key="$route.path"
-                            ></component>
-                        </transition>
-                    </router-view>
-
-                    <transition appear mode="out-in" name="navigation">
-                        <NavigationButton :key="$route.path">
-                        </NavigationButton>
-                    </transition>
-                </div>
-            </perfect-scrollbar>
-        </div> -->
     </div>
 </template>
 
@@ -68,16 +19,6 @@ export default {
             "saveState",
             "checkTotalVisit",
         ]),
-
-        /** Блокируем скролл при открытии меню */
-
-        // onGlobalScroll(event) {
-        //     if (this.menuState) {
-        //         let scrollContainer = this.$refs.globalScroll;
-        //         scrollContainer.$el.scrollTop =
-        //             scrollContainer.ps.lastScrollTop;
-        //     }
-        // },
     },
 
     computed: {
@@ -89,17 +30,6 @@ export default {
             "objectivs",
             "restorAPIobjectivs",
         ]),
-
-        // getMainWrapper() {
-        //     return {
-        //         main__wrapper: this.$route.name === "Page_2",
-        //     };
-        // },
-        // getMainContainer() {
-        //     return {
-        //         container__main: this.$route.name === "Page_2",
-        //     };
-        // },
     },
 
     mounted() {},
@@ -116,30 +46,6 @@ export default {
             }
         },
     },
-
-    // watch: {
-    //     start() {
-    //         if (this.start) {
-    //             this.addVisitPage(this.$route.name);
-    //             if (this.lastPage !== undefined && this.lastPage !== null) {
-    //                 console.log(this.lastPage, "8888");
-
-    //                 this.$router.push({ path: this.lastPage });
-    //             }
-    //             this.restorAPIobjectivs;
-    //         }
-    //     },
-    //     $route() {
-    //         console.log("1");
-    //         if (this.start) {
-    //             console.log("2");
-    //             this.addVisitPage(this.$route.name);
-    //             this.setLocation(this.$route.path);
-    //             this.saveState();
-    //             this.checkTotalVisit();
-    //         }
-    //     },
-    // },
 };
 </script>
 
